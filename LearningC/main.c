b6e0c0fbf4e6c5c3f1a9ea528f1560d9cbb34f35
du.yu <du.yu@qunar.com>
2017-05-09 15:04:53 +0800

    Initial Commit

---
+++ LearningC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LearningC
+//
+//  Created by qitmac000018 on 2017/5/9.
+//  Copyright © 2017年 qitmac000018. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
